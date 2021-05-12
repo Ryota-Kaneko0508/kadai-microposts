464a572680d6a55c9b34aede438839d28baa4c06
ryota.kaneko <kane2508ryo@gmail.com>
2021-05-12 11:53:29 +0000

    follow, unfollow

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_08_192037) do
+ActiveRecord::Schema.define(version: 2021_05_11_103004) do
 
   create_table "microposts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -18,6 +18,26 @@ ActiveRecord::Schema.define(version: 2021_05_08_192037) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
+
+  create_table "relationshhips", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "follow_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follow_id"], name: "index_relationshhips_on_follow_id"
+    t.index ["user_id", "follow_id"], name: "index_relationshhips_on_user_id_and_follow_id", unique: true
+    t.index ["user_id"], name: "index_relationshhips_on_user_id"
+  end
+
+  create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "follow_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follow_id"], name: "index_relationships_on_follow_id"
+    t.index ["user_id", "follow_id"], name: "index_relationships_on_user_id_and_follow_id", unique: true
+    t.index ["user_id"], name: "index_relationships_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -29,4 +49,8 @@ ActiveRecord::Schema.define(version: 2021_05_08_192037) do
   end
 
   add_foreign_key "microposts", "users"
+  add_foreign_key "relationshhips", "users"
+  add_foreign_key "relationshhips", "users", column: "follow_id"
+  add_foreign_key "relationships", "users"
+  add_foreign_key "relationships", "users", column: "follow_id"
 end
