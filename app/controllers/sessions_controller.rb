67e70614d1c2edd970a5a46d1cfd7fa19ef75cf5
ryota.kaneko <kane2508ryo@gmail.com>
2021-05-12 12:21:42 +0000

    timeline

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     password = params[:session][:password]
     if login(email, password)
       flash[:seccess] = 'ログインに成功しました。'
-      redirect_to '/'
+      redirect_to @user
     else
       flash.now[:danger] = 'ログインに失敗しました。'
       render :new
